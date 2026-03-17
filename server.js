425aa080628ee8af67213fbc29ba23481885b090
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-17 11:07:30 -0700

    Referencing Digital Ocean guide

--- server.js
+++ server.js
@@ -4,5 +4,10 @@ import fs from 'fs';
 http.createServer(function(req, res) 
 {
 if (req.url === "/")
-res.end = "home.html"
+fs.end = "home.html"
+else if (req.url === "/about")
+res.end = "This is a server." 
+else ()
+res.writeHead(404)
+res.end = "Not found"
 })
