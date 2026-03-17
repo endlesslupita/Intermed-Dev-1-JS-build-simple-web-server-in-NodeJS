eaf41220dced4624a39c7dd063cbd5a3ed836732
endlesslupita <endlesslupita@users.noreply.github.com>
2026-03-17 11:32:57 -0700

    Format and fill out details

--- server.js
+++ server.js
@@ -3,11 +3,24 @@ import fs from 'fs';
 
 http.createServer(function(req, res) 
 {
-if (req.url === "/")
-fs.end = "home.html"
-else if (req.url === "/about")
-res.end = "This is a server." 
-else ()
-res.writeHead(404)
-res.end = "Not found"
+    if (req.url === "/") 
+    {
+        fs.readFile("home.html", (err, data) => 
+        {if(err) 
+            {
+            console.err("Error reading file: ", err);
+            return;
+            }
+        })
+    }
+    else if (req.url === "/about") 
+    {
+        res.end("This is a server.") 
+    }
+
+    else 
+    {
+        res.writeHead(404)
+        res.end("Not found")
+    };
 })
